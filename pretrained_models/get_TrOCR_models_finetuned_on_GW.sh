818479fa1021ef20195dd86416ab3b17bafad3d9
Steryios Papazis <spapazis@cse.uoi.gr>
2025-08-13 17:29:51 +0300

    fixing flags

--- pretrained_models/get_TrOCR_models_finetuned_on_GW.sh
+++ pretrained_models/get_TrOCR_models_finetuned_on_GW.sh
@@ -3,6 +3,6 @@
 for f in {0..3};
 do
     echo "Downloading TrOCR finetuned on GW cv${f}..."
-    wget "https://github.com/stevepapazis/kws-semantic-reranking/releases/download/TrOCR_finetuned_on_GW_cv${f}/trocr_finetuned_on_GW_cv${f}.zip"
+    wget -nc "https://github.com/stevepapazis/kws-semantic-reranking/releases/download/TrOCR_finetuned_on_GW_cv${f}/trocr_finetuned_on_GW_cv${f}.zip"
     unzip -n "trocr_finetuned_on_GW_cv${f}.zip"
 done;
--- pretrained_models/get_WordRetrievalNet_pretrained_models.sh
+++ pretrained_models/get_WordRetrievalNet_pretrained_models.sh
@@ -3,17 +3,17 @@
 for f in {0..3};
 do
     echo "Downloading WordRetrievalNet pretrained on GW cv${f}..."
-    wget "https://github.com/stevepapazis/kws-semantic-reranking/releases/download/wordretrievalnet_gw_cv${f}/wordretrievalnet_gw_cv${f}.zip"
+    wget -nc "https://github.com/stevepapazis/kws-semantic-reranking/releases/download/WordRetrievalNet_pretrained_on_GW_cv${f}/wordretrievalnet_gw_cv${f}.zip"
     
-    unzip "wordretrievalnet_gw_cv${f}.zip"
-    mkdir -p "../../output/WordRetrievalNet/resnet50_GW${f}/checkpoint/"
-    mv "wordretrievalnet_gw_cv${f}/WordRetrievalNet_best.pth" "../../output/WordRetrievalNet/resnet50_GW${f}/checkpoint/WordRetrievalNet_best.pth"
+    unzip -o "wordretrievalnet_gw_cv${f}.zip"
+    mkdir -p "../output/WordRetrievalNet/resnet50_GW${f}/checkpoint/"
+    mv "WordRetrievalNet_best.pth" "../output/WordRetrievalNet/resnet50_GW${f}/checkpoint/WordRetrievalNet_best.pth"
 done;
 
 
 echo "Downloading WordRetrievalNet pretrained on IAM..."
-wget "https://github.com/stevepapazis/kws-semantic-reranking/releases/download/wordretrievalnet_iam/wordretrievalnet_iam.zip"
+wget -nc "https://github.com/stevepapazis/kws-semantic-reranking/releases/download/WordRetrievalNet_pretrained_on_IAM/wordretrievalnet_iam.zip"
 
-unzip "wordretrievalnet_iam.zip"
-mkdir -p "../../output/WordRetrievalNet/resnet50_IAM/checkpoint/"
-mv "wordretrievalnet_iam/WordRetrievalNet_best.pth" "../../output/WordRetrievalNet/resnet50_IAM/checkpoint/WordRetrievalNet_best.pth"
+unzip -o "wordretrievalnet_iam.zip"
+mkdir -p "../output/WordRetrievalNet/resnet50_IAM/checkpoint/"
+mv "WordRetrievalNet_best.pth" "../output/WordRetrievalNet/resnet50_IAM/checkpoint/WordRetrievalNet_best.pth"
